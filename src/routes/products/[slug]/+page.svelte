f96535926d989416bb64e286fe18e6badf106672
william-man <william.man@dxw.com>
2025-03-13 16:31:57 +0000

    Add params variable and insert into products page

--- src/routes/products/[slug]/+page.svelte
+++ src/routes/products/[slug]/+page.svelte
@@ -1,12 +1,11 @@
 <script>
 	import ProductGroup from "$lib/component/ProductGroup/ProductGroup.svelte";
-
-
+    const { data } = $props();
+    const current_category = data.current_category;
 </script>
 
 <div>
+    <h1>Category: {current_category}</h1>
     <ProductGroup />
 </div>
 
-<style>
-</style>
