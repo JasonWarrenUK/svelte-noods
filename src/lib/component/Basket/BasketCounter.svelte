ef302c2a6925dc36967dfaecb5fce45d40f12aa9
MarikaBBB <mbertelli012@gmail.com>
2025-03-13 16:42:35 +0000

    Feat: initial basket count

---
+++ src/lib/component/Basket/BasketCounter.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { Product } from '$lib/data/products';
+
+	let basket = $state<Product[]>([]);
+
+	const basketCount = $derived.by(() => basket.length);
+
+	export function addToBasket(product: Product) {
+		basket = [...basket, product];
+	}
+
+	export function clearBasket() {
+		basket = [];
+	}
+
+</script>
+
+<div class="basket-counter">
+	🛒
+	{#if basketCount > 0}
+		<span class="counter">{basketCount}</span>
+	{/if}
+</div>
